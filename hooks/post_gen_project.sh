9ae693ffa49b8d6b5f22436bf9afd351fad9f5f2
Stoo Johnston <scj@stooj.org>
2018-05-21 23:38:21 +0100

    Make sure origin repo is in git config

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -7,4 +7,5 @@ git config user.email "{{ cookiecutter.email_address }}"
 git commit -m 'Initial commit after generating formula from https://gitlab.com/stooj/saltstack-formula-cookiecutter'
 git push --set-upstream git@odo.stooj.org:team-ginstoo/$(git rev-parse --show-toplevel | xargs basename).git $(git rev-parse --abbrev-ref HEAD)
 git push --set-upstream git@gitlab.com:stooj/$(git rev-parse --show-toplevel | xargs basename).git $(git rev-parse --abbrev-ref HEAD)
+git remote add origin git@odo.stooj.org:team-ginstoo/$(git rev-parse --show-toplevel | xargs basename).git
 git remote add gitlab git@gitlab.com:stooj/$(git rev-parse --show-toplevel | xargs basename).git
