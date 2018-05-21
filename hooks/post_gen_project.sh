a3a9146b5efb02d2b7c7db4c6cc0ab654378e572
Stoo Johnston <scj@stooj.org>
2018-05-21 23:42:29 +0100

    Set odo to be upstream repo

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -5,7 +5,7 @@ git add -A .
 git config user.name "{{ cookiecutter.full_name }}"
 git config user.email "{{ cookiecutter.email_address }}"
 git commit -m 'Initial commit after generating formula from https://gitlab.com/stooj/saltstack-formula-cookiecutter'
-git push --set-upstream git@odo.stooj.org:team-ginstoo/$(git rev-parse --show-toplevel | xargs basename).git $(git rev-parse --abbrev-ref HEAD)
 git push --set-upstream git@gitlab.com:stooj/$(git rev-parse --show-toplevel | xargs basename).git $(git rev-parse --abbrev-ref HEAD)
+git push --set-upstream git@odo.stooj.org:team-ginstoo/$(git rev-parse --show-toplevel | xargs basename).git $(git rev-parse --abbrev-ref HEAD)
 git remote add origin git@odo.stooj.org:team-ginstoo/$(git rev-parse --show-toplevel | xargs basename).git
 git remote add gitlab git@gitlab.com:stooj/$(git rev-parse --show-toplevel | xargs basename).git
