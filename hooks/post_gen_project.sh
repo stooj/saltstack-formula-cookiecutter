6549a79a2d41f2c2642cca7a65481585f969a5c2
Stoo Johnston <scj@stooj.org>
2018-05-21 23:27:08 +0100

    Automatically push repos to host

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -4,4 +4,6 @@ git init
 git add -A .
 git config user.name "{{ cookiecutter.full_name }}"
 git config user.email "{{ cookiecutter.email_address }}"
-git commit -m 'Initial commit after generating formula from https://github.com/mitodl/saltstack-formula-cookiecutter'
+git commit -m 'Initial commit after generating formula from https://gitlab.com/stooj/saltstack-formula-cookiecutter'
+git push --set-upstream git@odo.stooj.org:team-ginstoo/$(git rev-parse --show-toplevel | xargs basename).git $(git rev-parse --abbrev-ref HEAD)
+git push --set-upstream git@gitlab.com:stooj/$(git rev-parse --show-toplevel | xargs basename).git $(git rev-parse --abbrev-ref HEAD)
